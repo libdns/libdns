89d6e0ed31978260ed35a67cd2cf5fd7da79f2c2
viscropst <bcct2010@live.cn>
2020-10-19 23:43:48 +0800

    _demo/demo.go:Change the way to get required info

--- alidns/_demo/demo.go
+++ alidns/_demo/demo.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 
 	al "github.com/libdns/alidns"
@@ -10,14 +12,26 @@ import (
 )
 
 func main() {
-	provider := al.Provider{
-		AccKeyID:     "LTAI4G3UTA4x2XRm8HrgGJ63",       //Modify your AccessKeyId here
-		AccKeySecret: "wO0q5OUKPWg8Iuy63VNuxLsdHGSH6d", //Modify your AccessKeySecret here
+	accKeyID := strings.TrimSpace(os.Getenv("ACCESS_KEY_ID"))
+	accKeySec := strings.TrimSpace(os.Getenv("ACCESS_KEY_SECRET"))
+	zone := strings.TrimSpace(os.Args[1])
+	if (accKeyID == "") || (accKeySec == "") {
+		fmt.Printf("ERROR: %s\n", "ACCESS_KEY_ID or ACCESS_KEY_SECRET missing")
+		return
 	}
-	zone := "viscrop.top" //Modify your Zone here
+	if zone == "" {
+		fmt.Printf("ERROR: %s\n", "First arg zone missing")
+		return
+	}
+	fmt.Printf("Get ACCESS_KEY_ID: %s,ACCESS_KEY_SECRET: %s,ZONE: %s\n", accKeyID, accKeySec, zone)
+	provider := al.Provider{
+		AccKeyID:     accKeyID,
+		AccKeySecret: accKeySec,
+	}
 	records, err := provider.GetRecords(context.TODO(), zone)
 	if err != nil {
 		fmt.Printf("ERROR: %s\n", err.Error())
+		return
 	}
 	testName := "_libdns_test"
 	testID := ""
@@ -41,6 +55,7 @@ func main() {
 		}
 		if err != nil {
 			fmt.Printf("ERROR: %s\n", err.Error())
+			return
 		}
 	}
 
@@ -48,7 +63,7 @@ func main() {
 	fmt.Scanln()
 	if testID != "" {
 		fmt.Println("Replacing entry for ", testName)
-		_, err = provider.SetRecords(context.TODO(), zone, []l.Record{l.Record{
+		records, err = provider.SetRecords(context.TODO(), zone, []l.Record{l.Record{
 			Type:  "TXT",
 			Name:  testName,
 			Value: fmt.Sprintf("Replacement test entry created by libdns %s", time.Now()),
@@ -57,6 +72,7 @@ func main() {
 		}})
 		if err != nil {
 			fmt.Printf("ERROR: %s\n", err.Error())
+			return
 		}
 	}
 	fmt.Println("Press any Key to delete the test entry")
@@ -65,6 +81,7 @@ func main() {
 	_, err = provider.DeleteRecords(context.TODO(), zone, records)
 	if err != nil {
 		fmt.Printf("ERROR: %s\n", err.Error())
+		return
 	}
 
 }
