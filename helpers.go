b8a57502fec1e0b47b617a03394e10380ccb7527
Jan Wiebe <janwiebe@jump.nl>
2025-04-04 09:06:13 +0200

    remove unused method

--- helpers.go
+++ helpers.go
@@ -2,19 +2,7 @@ package mijnhost
 
 import (
 	"strings"
-
-	"github.com/libdns/libdns"
 )
-
-func isRecordExists(records []libdns.Record, libRecord libdns.Record) bool {
-	for _, record := range records {
-		if libRecord.ID == record.ID || (libRecord.Name == record.Name && libRecord.Type == record.Type) {
-			return true
-		}
-	}
-
-	return false
-}
 
 func normalizeZone(zone string) string {
 	return strings.TrimSuffix(strings.Replace(zone, "*.", "", 1), ".")
